49d3503e7e11b591be8304dca7715400c07b365e
Lorenzo Pacchiardi <lorenzo.pacchiardi@stats.ox.ac.uk>
2024-08-22 15:31:18 +0100

    Update cost per token

--- full_processing_steps/src/utils.py
+++ full_processing_steps/src/utils.py
@@ -4,40 +4,48 @@ import os
 
 import pandas as pd
 
-cost_per_token = {"gpt-4-0125-preview": 0.01e-3,
-                  "gpt-4-1106-preview": 0.01e-3,
-                  "gpt-4-0613": 0.03e-3,
-                  "gpt-4-32k-0613": 0.06e-3,
+# THIS REFERS TO INPUT TOKENS AS THOSE ARE THE MAJORITY OF THE TOKENS IN OUR TESTS.
+# see https://openai.com/api/pricing/
+cost_per_token = {
+    "gpt-4o-2024-08-06": 0.0025e-3,
+    "gpt-4o-2024-05-13": 0.005e-3,
+    "gpt-4o-mini-2024-07-18": 0.00015e-3,
 
-                  "gpt-3.5-turbo-0125": 0.0005e-3,  # optimized for chat
-                  "gpt-3.5-turbo-1106": 0.001e-3,  # optimized for chat
-                  "gpt-3.5-turbo-instruct": 0.0015e-3,
-                  # instruct model, indicated as the replacement for old instructGPT models (similar capabilities to
-                  #  textx-davinci-003, compatible with legacy Completions endpoint)
+    "gpt-4-turbo-2024-04-09": 0.01e-3,
+    "gpt-4-0125-preview": 0.01e-3,
+    "gpt-4-1106-preview": 0.01e-3,
+    "gpt-4-0613": 0.03e-3,
+    "gpt-4-32k-0613": 0.06e-3,
 
-                  # older models, will be deprecated in June 24
-                  "gpt-4-0314": 0.03e-3,
-                  "gpt-4-32k-0314": 0.06e-3,
-                  "gpt-3.5-turbo-0613": 0.0015e-3,
-                  "gpt-3.5-turbo-16k-0613": 0.003e-3,
-                  "gpt-3.5-turbo-0301": 0.0015e-3,
+    "gpt-3.5-turbo-0125": 0.0005e-3,  # optimized for chat
+    "gpt-3.5-turbo-1106": 0.001e-3,  # optimized for chat
+    "gpt-3.5-turbo-instruct": 0.0015e-3,
+    # instruct model, indicated as the replacement for old instructGPT models (similar capabilities to
+    #  textx-davinci-003, compatible with legacy Completions endpoint)
 
-                  # even older models
-                  "text-ada-001": 0.0004e-3,
-                  "text-babbage-001": 0.0005e-3,
-                  "text-curie-001": 0.0020e-3,
-                  "text-davinci-001": 0.0200e-3,
-                  "text-davinci-002": 0.0200e-3,
-                  "text-davinci-003": 0.0200e-3,
+    # older models, will be deprecated in June 24
+    "gpt-4-0314": 0.03e-3,
+    "gpt-4-32k-0314": 0.06e-3,
+    "gpt-3.5-turbo-0613": 0.0015e-3,
+    "gpt-3.5-turbo-16k-0613": 0.003e-3,
+    "gpt-3.5-turbo-0301": 0.0015e-3,
 
-                  "ada": 0.0004e-3,
-                  "babbage": 0.0005e-3,
-                  "curie": 0.0020e-3,
-                  "davinci": 0.0200e-3,
-                  }
+    # even older models
+    "text-ada-001": 0.0004e-3,
+    "text-babbage-001": 0.0005e-3,
+    "text-curie-001": 0.0020e-3,
+    "text-davinci-001": 0.0200e-3,
+    "text-davinci-002": 0.0200e-3,
+    "text-davinci-003": 0.0200e-3,
+
+    "ada": 0.0004e-3,
+    "babbage": 0.0005e-3,
+    "curie": 0.0020e-3,
+    "davinci": 0.0200e-3,
+}
 
 
-# This dictionary is up-to-date as 2024-03-20
+# This dictionary is up-to-date as 2024-08-22.
 
 
 def compute_n_tokens(string, tokenizer):
